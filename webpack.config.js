4383c6f606ef113fe25603d9952e81a871775df5
Leonid <zemlyanykh@bk.ru>
2025-01-28 16:33:40 +0500

    Add babel

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { clear } = require('console')
+const path = require('path')
+
+module.exports = {
+    mode: 'production',
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
